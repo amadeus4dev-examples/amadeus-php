aa2712bbdeade752e40be4889a12560923ab1ca4
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-03-22 09:32:47 +0100

    add constant for supporting the api with extra header

---
+++ src/Constants.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Amadeus;
+
+class Constants
+{
+    const API_NEED_EXTRA_HEADER = array(
+        "/v1/shopping/availability/flight-availabilities"
+    );
+
+}
