dfbfa4d87973e7565c9e8db8bf4f0bdcd579e836
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-06-07 16:18:28 +0200

    remove automatically refresh token check functions

--- src/client/AccessToken.php
+++ src/client/AccessToken.php
@@ -11,7 +11,8 @@ use Amadeus\Request;
 class AccessToken
 {
     private ?string $access_token = null;
-    private ?int $expires_at = null;
+    private ?int $expires_in = null;
+    //private ?int $expires_at = null;
 
     private HTTPClient $client;
 
@@ -23,45 +24,53 @@ class AccessToken
         $this->client = $client;
     }
 
+//    public function updateAccessToken(): void
+//    {
+//        // Checks if it is the first time to fetch access token
+//        if ($this->access_token != null) {
+//            // Checks if the current access token expires.
+//            if ($this->expires_at < time()) {
+//                // If expired then refresh the token
+//                $this->storeAccessToken($this->fetchAccessToken());
+//
+//                file_put_contents(
+//                    'php://stdout',
+//                    "Access token expired ! Automatically update access token -> [". $this->access_token ."] !"."\n"
+//                );
+//            } else {
+//                // Else still return the current token
+//
+//                file_put_contents(
+//                    'php://stdout',
+//                    "Current access token -> [". $this->access_token ."] is still available !"."\n"
+//                );
+//            }
+//        } else {
+//            // First time to fetch access token
+//            $this->storeAccessToken($this->fetchAccessToken());
+//
+//            file_put_contents(
+//                'php://stdout',
+//                "First time to fetch access token -> [". $this->access_token ."] !"."\n"
+//            );
+//        }
+//    }
+
     public function updateAccessToken(): void
     {
-        // Checks if it is the first time to fetch access token
-        if ($this->access_token != null) {
-            // Checks if the current access token expires.
-            if ($this->expires_at < time()) {
-                // If expired then refresh the token
-                $this->storeAccessToken($this->fetchAccessToken());
-
-                file_put_contents(
-                    'php://stdout',
-                    "Access token expired ! Automatically update access token -> [". $this->access_token ."] !"."\n"
-                );
-            } else {
-                // Else still return the current token
-
-                file_put_contents(
-                    'php://stdout',
-                    "Current access token -> [". $this->access_token ."] is still available !"."\n"
-                );
-            }
-        } else {
-            // First time to fetch access token
+        if ($this->access_token == null) {
             $this->storeAccessToken($this->fetchAccessToken());
-
-            file_put_contents(
-                'php://stdout',
-                "First time to fetch access token -> [". $this->access_token ."] !"."\n"
-            );
         }
     }
 
     protected function storeAccessToken(object $object)
     {
         $this->access_token = $object->access_token;
+        $this->expires_in = $object->expires_in;
 
         // Renew the token 10 seconds earlier than required
         // Cuz the token will expire in 1799 seconds
-        $this->expires_at = time() + $object->expires_in - 10;
+        //$this->expires_at = time() + $object->expires_in - 10;
     }
 
     public function fetchAccessToken(): ?object
@@ -103,19 +112,24 @@ class AccessToken
         $this->access_token = $access_token;
     }
 
-    /**
-     * @param int|null $expires_at
-     */
-    public function setExpiresAt(?int $expires_at): void
+    public function getExpiresIn(): int
     {
-        $this->expires_at = $expires_at;
+        return $this->expires_in;
     }
 
-    /**
-     * @return int
-     */
-    public function getExpiresAt(): int
-    {
-        return $this->expires_at;
-    }
+//    /**
+//     * @param int|null $expires_at
+//     */
+//    public function setExpiresAt(?int $expires_at): void
+//    {
+//        $this->expires_at = $expires_at;
+//    }
+//
+//    /**
+//     * @return int
+//     */
+//    public function getExpiresAt(): int
+//    {
+//        return $this->expires_at;
+//    }
 }
--- tests/AccessTokenTest.php
+++ tests/AccessTokenTest.php
@@ -19,7 +19,6 @@ use ReflectionException;
 final class AccessTokenTest extends TestCase
 {
     private BasicHTTPClient $client;
-    private object $result;
 
     /**
      * @Before
@@ -37,13 +36,13 @@ final class AccessTokenTest extends TestCase
             ->method("getConfiguration")
             ->willReturn($configuration);
 
-        $this->result = (object) [
+        $result = (object) [
             "access_token" => "my_token",
             "expires_in" => 1799
         ];
 
         $accessToken = new AccessToken($this->client);
-        PHPUnitUtil::callMethod($accessToken, "storeAccessToken", array($this->result));
+        PHPUnitUtil::callMethod($accessToken, "storeAccessToken", array($result));
 
         $this->client->expects($this->any())
             ->method("getAccessToken")
@@ -53,48 +52,49 @@ final class AccessTokenTest extends TestCase
     public function testParseAccessToken(): void
     {
         $accessToken = $this->client->getAccessToken();
-        $this->assertEquals(time()+1789, $accessToken->getExpiresAt());
+        $this->assertEquals(1799, $accessToken->getExpiresIn());
+        //$this->assertEquals(time()+1799, $accessToken->getExpiresAt());
         $this->assertEquals('my_token', $accessToken->getBearerToken());
     }
 
     public function testSetter(): void
     {
         $accessToken = $this->client->getAccessToken();
-        $accessToken->setExpiresAt(time()+1789);
+        //$accessToken->setExpiresAt(time()+1789);
         $accessToken->setBearerToken("new_token");
-        $this->assertEquals(time()+1789, $accessToken->getExpiresAt());
+        //$this->assertEquals(time()+1789, $accessToken->getExpiresAt());
         $this->assertEquals('new_token', $accessToken->getBearerToken());
     }
 
-    public function testFetchAccessTokenWhenNotExpired(): void
-    {
-        $obj = $this->getMockBuilder(AccessToken::class)
-            ->setConstructorArgs(array($this->client))
-            ->onlyMethods(array("fetchAccessToken"))
-            ->getMock();
-
-        $obj->expects($this->exactly(1))
-            ->method('fetchAccessToken')
-            ->willReturn($this->result);
-
-        $obj->getBearerToken();
-        $obj->getBearerToken();
-    }
-
-    public function testUpdateAccessTokenWhenExpired(): void
-    {
-        $obj = $this->getMockBuilder(AccessToken::class)
-            ->setConstructorArgs(array($this->client))
-            ->onlyMethods(array("fetchAccessToken"))
-            ->getMock();
-
-        $this->result->expires_in = -1;
-
-        $obj->expects($this->exactly(2))
-            ->method('fetchAccessToken')
-            ->willReturn($this->result);
-
-        $obj->getBearerToken();
-        $obj->getBearerToken();
-    }
+//    public function testFetchAccessTokenWhenNotExpired(): void
+//    {
+//        $obj = $this->getMockBuilder(AccessToken::class)
+//            ->setConstructorArgs(array($this->client))
+//            ->onlyMethods(array("fetchAccessToken"))
+//            ->getMock();
+//
+//        $obj->expects($this->exactly(1))
+//            ->method('fetchAccessToken')
+//            ->willReturn($this->result);
+//
+//        $obj->getBearerToken();
+//        $obj->getBearerToken();
+//    }
+//
+//    public function testUpdateAccessTokenWhenExpired(): void
+//    {
+//        $obj = $this->getMockBuilder(AccessToken::class)
+//            ->setConstructorArgs(array($this->client))
+//            ->onlyMethods(array("fetchAccessToken"))
+//            ->getMock();
+//
+//        $this->result->expires_in = -1;
+//
+//        $obj->expects($this->exactly(2))
+//            ->method('fetchAccessToken')
+//            ->willReturn($this->result);
+//
+//        $obj->getBearerToken();
+//        $obj->getBearerToken();
+//    }
 }
