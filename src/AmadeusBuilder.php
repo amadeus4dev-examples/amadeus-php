fcc3ed069bb4b6887e2c7c37c88abfd9f01c7ed4
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-06-08 18:21:33 +0200

    update php doc

--- src/AmadeusBuilder.php
+++ src/AmadeusBuilder.php
@@ -20,7 +20,7 @@ class AmadeusBuilder
 
     /**
      * @param bool $ssl
-     * @return $this
+     * @return AmadeusBuilder
      */
     public function setSsl(bool $ssl): AmadeusBuilder
     {
@@ -28,24 +28,38 @@ class AmadeusBuilder
         return $this;
     }
 
+    /**
+     * @param int $port
+     * @return AmadeusBuilder
+     */
     public function setPort(int $port): AmadeusBuilder
     {
         $this->configuration->setPort($port);
         return $this;
     }
 
+    /**
+     * @param HTTPClient $httpClient
+     * @return AmadeusBuilder
+     */
     public function setHttpClient(HTTPClient $httpClient): AmadeusBuilder
     {
         $this->configuration->setHttpClient($httpClient);
         return $this;
     }
 
+    /**
+     * @return AmadeusBuilder
+     */
     public function setProductionEnvironment(): AmadeusBuilder
     {
         $this->configuration->setProductionEnvironment();
         return $this;
     }
 
+    /**
+     * @return Amadeus
+     */
     public function build(): Amadeus
     {
         return new Amadeus($this->configuration);
