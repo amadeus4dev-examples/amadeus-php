a291de7d43f3fc4b31de3481ce13f53a36bfb001
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-07-20 17:05:38 +0200

    fix mistaken import

--- src/Shopping.php
+++ src/Shopping.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Amadeus;
 
-use Amadeus\Resources\FlightDestination;
 use Amadeus\Shopping\Availability;
 use Amadeus\Shopping\FlightDates;
 use Amadeus\Shopping\FlightDestinations;
