cbe7aa4f9d7bce112ad15e55d7227d1717178d3a
MinjiK <km.minjikim@gmail.com>
2022-07-29 09:51:21 +0200

    Update Location.php
    added URL of Airport nearest relevant API

--- src/resources/Location.php
+++ src/resources/Location.php
@@ -7,8 +7,9 @@ namespace Amadeus\Resources;
 use Amadeus\ReferenceData\Locations\Airports;
 
 /**
- * A Location object as returned by the Airport and City Search API, Airport Nearest Relevant API.
+ * A Location object as returned by the Airport and City Search API, Airport Nearest Relevant API and Travel Recommendations API .
  * @see Locations::get()
+ * @see https://developers.amadeus.com/self-service/category/air/api-doc/airport-nearest-relevant
  * @see Airports::get()
  * @see https://developers.amadeus.com/self-service/category/air/api-doc/airport-and-city-search/api-reference
  * @see RecommendedLocations::get()
